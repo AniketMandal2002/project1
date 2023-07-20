cd515500934b60b2e4d84640986f15fbae4b260a
AniketMandal2002 <mandalaniket086@gmail.com>
2023-07-21 00:42:02 +0530

    C3

--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -6,8 +6,9 @@ const DB=process.env.LOCAL_DATABASE;
 mongoose.connect(DB,{
 
  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+ 
+  useUnifiedTopology: true
+  
 }).then(()=>{
     console.log("Connected to MongoDB");
 }).catch(err=>{
