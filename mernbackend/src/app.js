88a5d5e89006602783479fcc63e024817a3e1270
AniketMandal2002 <mandalaniket086@gmail.com>
2023-07-21 00:57:04 +0530

    C4

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,6 +1,10 @@
 //type nul > app.js
 //const static_path = path.join(__dirname,"../public")
 //app.use(express.static(static_path))
+const mongoose = require('mongoose');
+
+
+
 
 const express = require("express");
 const path = require("path");
@@ -13,6 +17,20 @@ require("./db/conn");
 dotenv.config({ path: "./config.env" });
 
 app.use(express.urlencoded({ extended: true }));
+
+const DB=process.env.LOCAL_DATABASE;
+
+mongoose.connect(DB,{
+
+ useNewUrlParser: true,
+ 
+  useUnifiedTopology: true
+  
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err=>{
+    console.log(err);
+})
 
 const static_path = path.join(__dirname, "../public");
 const templete_path = path.join(__dirname, "../templetes/views");
